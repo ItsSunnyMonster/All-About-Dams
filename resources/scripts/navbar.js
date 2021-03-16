d8648c44eeb2f1e5b300b3313324fd8cdceab3d3
SunnyMonster123 <tonylin12345@outlook.com>
2021-03-17 08:53:09 +1300

    Fix typo and delete navbar.js

--- resources/scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll(".link");
-
-links.forEach((link) => {
-  link.classList.remove("active");
-  if (window.location.href.includes(link.getAttribute("href"))) {
-    link.classList.add("active");
-  }
-});
