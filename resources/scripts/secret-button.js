b5ebee2a86a3ab39f4123a13b7f396405287790f
SunnyMonster123 <tonylin12345@outlook.com>
2021-03-18 19:00:14 +1300

    Improved things

---
+++ resources/scripts/secret-button.js
@@ -0,0 +1,9 @@
+var secretButton = document.querySelector(".secret-btn");
+
+secretButton.addEventListener("click", () => {
+  document.title = "Did you notice that I was changed? ";
+  secretButton.innerHTML = "Oh. I didn't do anything. OR did I? ";
+  setTimeout(() => {
+    secretButton.blur();
+  }, 5000);
+});
